7eab35da0ca5f6b9fce61317e71e00d987bb4cb3
Abdulrahman Mardinli <34406204+aboodma@users.noreply.github.com>
2021-02-08 15:15:39 +0300

    update

--- app/Board.php
+++ app/Board.php
@@ -19,20 +19,20 @@ class Board extends Model
 
     public function getQrcodeAttribute(){
         $user=User::find($this->user_id);
-        $url=URL::to('/').'/menu/'.$user->slug.'/'.$this->slug;
+        $url=url('/').'/menu/'.$user->slug.'/'.$this->slug;
         $png=base64_encode(QrCode::format('png')->size(512)->generate($url));
 
         return $png;
     }
     public function getPrintQrcodeAttribute(){
         $user=User::find($this->user_id);
-        $url=URL::to('/').'/menu/'.$user->slug.'/'.$this->slug;
+        $url=url('/').'/menu/'.$user->slug.'/'.$this->slug;
         return QrCode::size(250)->generate($url);
 
     }
     public function getQrlinkAttribute(){
         $user=User::find($this->user_id);
-        $url=URL::to('/').'/menu/'.$user->slug.'/'.$this->slug;
+        $url=url('/').'/menu/'.$user->slug.'/'.$this->slug;
 
         return $url;
     }
